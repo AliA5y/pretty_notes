bdbfa0aecb8b1fc7da6c612672ac4a5e23649b86
AliA5y <ali.route9@gmail.com>
2024-07-21 17:39:32 +0300

    added home view and home view body

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pretty_notes/views/home.dart';
 
 void main() {
   runApp(const PrettyNotesApp());
@@ -9,6 +10,8 @@ class PrettyNotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return const MaterialApp(
+      home: HomeView(),
+    );
   }
 }
