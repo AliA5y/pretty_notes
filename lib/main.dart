51ebe28854812578eec825bca33d023438f5f78a
AliA5y <ali.route9@gmail.com>
2024-07-21 16:28:00 +0300

    initial commit, initialize project and added hive dependency packages

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const PrettyNotesApp());
+}
+
+class PrettyNotesApp extends StatelessWidget {
+  const PrettyNotesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
