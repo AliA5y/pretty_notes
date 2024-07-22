2bd94fb88e653035032edc7b0550a596b2d5ec87
AliA5y <ali.route9@gmail.com>
2024-07-22 10:43:24 +0300

    add edit action to add note sheet

--- lib/views/widgets/note_card.dart
+++ lib/views/widgets/note_card.dart
@@ -8,43 +8,58 @@ class NoteCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 6),
-      child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 28, vertical: 8),
-          decoration: const BoxDecoration(
-            color: Colors.teal,
-            borderRadius: BorderRadius.all(
-              Radius.circular(16),
+      child: GestureDetector(
+        onTap: () {},
+        child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 28, vertical: 8),
+            decoration: const BoxDecoration(
+              color: Colors.teal,
+              borderRadius: BorderRadius.all(
+                Radius.circular(16),
+              ),
             ),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              ListTile(
-                contentPadding: const EdgeInsets.all(0),
-                title: const Text(
-                  'Note Title',
-                  style: TextStyle(fontSize: 28),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                ListTile(
+                  contentPadding: const EdgeInsets.all(0),
+                  title: const Text(
+                    'Note Title',
+                    style: TextStyle(fontSize: 28),
+                  ),
+                  subtitle: const Text(
+                    'Note Text kjklfg lkdjfg lkdjfglkjsd glkdjglk dglksdjgklds gldskjg sdglkjdsklg dslkjglksd glkdsjglk sdglksdjg ',
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(color: Colors.white38),
+                  ),
+                  trailing: IconButton(
+                      padding: const EdgeInsets.all(8),
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.edit,
+                        size: 26,
+                      )),
                 ),
-                subtitle: const Text(
-                  'Note Text kjklfg lkdjfg lkdjfglkjsd glkdjglk dglksdjgklds gldskjg sdglkjdsklg dslkjglksd glkdsjglk sdglksdjg ',
-                  overflow: TextOverflow.ellipsis,
-                  style: TextStyle(color: Colors.white38),
+                const SizedBox(height: 10),
+                Row(
+                  children: [
+                    const Text(
+                      'May, 22, 2024',
+                      style: TextStyle(color: Colors.white38),
+                    ),
+                    const Spacer(),
+                    IconButton(
+                        padding: const EdgeInsets.all(8),
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.delete,
+                          size: 26,
+                        )),
+                  ],
                 ),
-                trailing: IconButton(
-                    padding: const EdgeInsets.all(8),
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.delete,
-                      size: 30,
-                    )),
-              ),
-              const SizedBox(height: 10),
-              const Text(
-                'May, 22, 2024',
-                style: TextStyle(color: Colors.white38),
-              ),
-            ],
-          )),
+              ],
+            )),
+      ),
     );
   }
 }
