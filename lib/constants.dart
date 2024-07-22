43b8d97e399c9ec89428901cd56216f49fb91446
AliA5y <ali.route9@gmail.com>
2024-07-22 09:33:23 +0300

    initialize hive

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,5 @@
 import 'package:flutter/material.dart';
 
 const kPremaryColor = Colors.teal;
+
+const kNotesBox = 'note_box';
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:pretty_notes/constants.dart';
 import 'package:pretty_notes/views/home.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox(kNotesBox);
   runApp(const PrettyNotesApp());
 }
 
