5f124d9e7121642195ba826a7178b5dbcf4f5dfe
AliA5y <ali.route9@gmail.com>
2024-07-22 18:00:38 +0300

    modify the colors list

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,11 +4,8 @@ const kPremaryColor = Colors.teal;
 
 const kNotesBox = 'note_box';
 List<Color> colors = [
-  Color(0xFF333652),
+  Color.fromARGB(255, 36, 39, 70),
   Color(0xFF0081a7),
-  Color(0xFF386641),
-  Color(0xFF778da9),
-  Color(0xFF8338EC),
+  Color.fromARGB(255, 46, 13, 96),
   Color(0xFF3A86FF),
-  Color(0xFF9d0208),
 ];
