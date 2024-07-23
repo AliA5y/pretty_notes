6b4beb9db8b6db4af61e74ac010d1cf43f92c06e
AliA5y <ali.route9@gmail.com>
2024-07-23 09:55:17 +0300

    move add note form to a separated file

--- lib/views/widgets/add_note_sheet.dart
+++ lib/views/widgets/add_note_sheet.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:pretty_notes/views/widgets/add_note_form.dart';
 import 'package:pretty_notes/views/widgets/custom_text_form_field.dart';
 import 'package:pretty_notes/views/widgets/submit_button.dart';
 
@@ -19,59 +20,6 @@ class AddNoteSheet extends StatelessWidget {
         child: const SingleChildScrollView(
           child: AddNoteForm(),
         ),
-      ),
-    );
-  }
-}
-
-class AddNoteForm extends StatefulWidget {
-  const AddNoteForm({
-    super.key,
-  });
-
-  @override
-  State<AddNoteForm> createState() => _AddNoteFormState();
-}
-
-class _AddNoteFormState extends State<AddNoteForm> {
-  final GlobalKey<FormState> formKey = GlobalKey();
-  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
-  String? title, content;
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-      autovalidateMode: autovalidateMode,
-      key: formKey,
-      child: Column(
-        children: [
-          CustomTextFormField(
-            onSaved: (value) {
-              title = value;
-            },
-            hint: 'Note title',
-          ),
-          const SizedBox(height: 16),
-          CustomTextFormField(
-            onSaved: (value) {
-              content = value;
-            },
-            hint: 'Note content',
-            maxLength: 12,
-          ),
-          const SizedBox(height: 16),
-          SubmitButton(
-            label: 'Add Note',
-            onTap: () {
-              if (formKey.currentState!.validate()) {
-                formKey.currentState!.save();
-              } else {
-                autovalidateMode = AutovalidateMode.always;
-                setState(() {});
-              }
-            },
-          ),
-          const SizedBox(height: 10),
-        ],
       ),
     );
   }
