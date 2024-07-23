f980b22cb007773c530e1a7781cce9a150bde7c4
AliA5y <ali.route9@gmail.com>
2024-07-23 10:07:45 +0300

    provide the cubit and add absorber

--- lib/views/widgets/add_note_form.dart
+++ lib/views/widgets/add_note_form.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pretty_notes/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:pretty_notes/views/widgets/custom_text_form_field.dart';
 import 'package:pretty_notes/views/widgets/submit_button.dart';
 
@@ -17,42 +19,57 @@ class _AddNoteFormState extends State<AddNoteForm> {
   String? title, content;
   @override
   Widget build(BuildContext context) {
-    return Form(
-      autovalidateMode: autovalidateMode,
-      key: formKey,
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            CustomTextFormField(
-              onSaved: (value) {
-                title = value;
-              },
-              hint: 'Note title',
+    return BlocConsumer<AddNoteCubit, AddNoteState>(
+      listener: (context, state) {
+        if (state is AddNoteFailur) {
+          print('Operation failed: ${state.errMessage}');
+        }
+        if (state is AddNoteSuccess) {
+          Navigator.pop(context);
+        }
+      },
+      builder: (context, state) {
+        return AbsorbPointer(
+          absorbing: state is AddNoteLoading ? true : false,
+          child: Form(
+            autovalidateMode: autovalidateMode,
+            key: formKey,
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  CustomTextFormField(
+                    onSaved: (value) {
+                      title = value;
+                    },
+                    hint: 'Note title',
+                  ),
+                  const SizedBox(height: 16),
+                  CustomTextFormField(
+                    onSaved: (value) {
+                      content = value;
+                    },
+                    hint: 'Note content',
+                    maxLength: 12,
+                  ),
+                  const SizedBox(height: 16),
+                  SubmitButton(
+                    label: 'Add Note',
+                    onTap: () {
+                      if (formKey.currentState!.validate()) {
+                        formKey.currentState!.save();
+                      } else {
+                        autovalidateMode = AutovalidateMode.always;
+                        setState(() {});
+                      }
+                    },
+                  ),
+                  const SizedBox(height: 10),
+                ],
+              ),
             ),
-            const SizedBox(height: 16),
-            CustomTextFormField(
-              onSaved: (value) {
-                content = value;
-              },
-              hint: 'Note content',
-              maxLength: 12,
-            ),
-            const SizedBox(height: 16),
-            SubmitButton(
-              label: 'Add Note',
-              onTap: () {
-                if (formKey.currentState!.validate()) {
-                  formKey.currentState!.save();
-                } else {
-                  autovalidateMode = AutovalidateMode.always;
-                  setState(() {});
-                }
-              },
-            ),
-            const SizedBox(height: 10),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
--- lib/views/widgets/add_note_sheet.dart
+++ lib/views/widgets/add_note_sheet.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pretty_notes/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:pretty_notes/views/widgets/add_note_form.dart';
 
 class AddNoteSheet extends StatelessWidget {
@@ -8,14 +10,17 @@ class AddNoteSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Padding(
-        padding: EdgeInsets.only(
-            top: 32,
-            left: 16,
-            right: 16,
-            bottom: MediaQuery.of(context).viewInsets.bottom),
-        child: const AddNoteForm(),
+    return BlocProvider(
+      create: (context) => AddNoteCubit(),
+      child: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.only(
+              top: 32,
+              left: 16,
+              right: 16,
+              bottom: MediaQuery.of(context).viewInsets.bottom),
+          child: const AddNoteForm(),
+        ),
       ),
     );
   }
