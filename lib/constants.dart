e2c9722b0efd1ff4e1bb07efbf5412ac715417b3
AliA5y <ali.route9@gmail.com>
2024-08-11 15:59:57 +0300

    modify colors list values

--- lib/constants.dart
+++ lib/constants.dart
@@ -8,6 +8,6 @@ List<Color> colors = [
   const Color(0xFF0081a7),
   const Color.fromARGB(255, 46, 13, 96),
   const Color(0xFF3A86FF),
-  const Color.fromARGB(255, 242, 160, 28),
-  const Color.fromARGB(255, 237, 35, 156),
+  const Color.fromARGB(255, 205, 63, 12),
+  const Color.fromARGB(255, 201, 3, 73),
 ];
