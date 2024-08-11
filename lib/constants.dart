b273c982c8e8da4b437575d591c4fc5d33bb2c9a
AliA5y <ali.route9@gmail.com>
2024-08-11 12:41:13 +0300

    complete the color list and modify the colors

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,8 +4,10 @@ const kPremaryColor = Colors.teal;
 
 const kNotesBox = 'note_box';
 List<Color> colors = [
-  Color.fromARGB(255, 36, 39, 70),
-  Color(0xFF0081a7),
-  Color.fromARGB(255, 46, 13, 96),
-  Color(0xFF3A86FF),
+  const Color.fromARGB(255, 16, 21, 77),
+  const Color(0xFF0081a7),
+  const Color.fromARGB(255, 46, 13, 96),
+  const Color(0xFF3A86FF),
+  const Color.fromARGB(255, 242, 160, 28),
+  const Color.fromARGB(255, 237, 35, 156),
 ];
--- lib/views/widgets/colors_list.dart
+++ lib/views/widgets/colors_list.dart
@@ -1,16 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:pretty_notes/constants.dart';
 
-class ColorsList extends StatelessWidget {
+class ColorsList extends StatefulWidget {
   const ColorsList({
     super.key,
   });
 
   @override
+  State<ColorsList> createState() => _ColorsListState();
+}
+
+class _ColorsListState extends State<ColorsList> {
+  int currentIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
     return ListView.builder(
+      itemCount: colors.length,
       scrollDirection: Axis.horizontal,
       itemBuilder: (context, index) {
-        return const ColorItem();
+        return GestureDetector(
+            onTap: () {
+              currentIndex = index;
+              setState(() {});
+            },
+            child: ColorItem(
+                color: colors[index], isSelected: currentIndex == index));
       },
     );
   }
@@ -19,13 +34,23 @@ class ColorsList extends StatelessWidget {
 class ColorItem extends StatelessWidget {
   const ColorItem({
     super.key,
+    required this.isSelected,
+    required this.color,
   });
-
+  final bool isSelected;
+  final Color color;
   @override
   Widget build(BuildContext context) {
-    return const CircleAvatar(
-      radius: 38,
-      backgroundColor: Colors.amber,
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 4),
+      child: CircleAvatar(
+        radius: 38,
+        backgroundColor: isSelected ? Theme.of(context).highlightColor : color,
+        child: CircleAvatar(
+          radius: 32,
+          backgroundColor: color,
+        ),
+      ),
     );
   }
 }
